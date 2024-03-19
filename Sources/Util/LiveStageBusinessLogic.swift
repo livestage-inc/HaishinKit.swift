e70b6266e362f7f738e865f67bc1efc2d6691353
pawan-100ms <pawan@100ms.live>
2024-03-19 17:42:32 +0400

    Storing photos in half a second. up to a minute

--- Sources/Util/LiveStageBusinessLogic.swift
+++ Sources/Util/LiveStageBusinessLogic.swift
@@ -169,7 +169,7 @@ public class LiveStageFastStorage {
                             
                             self.highResFrameCaptures.append(HighResFrameCapture(timestamp: timestamp, data: data as Data))
                             
-                            if self.highResFrameCaptures.count > 600 {
+                            if self.highResFrameCaptures.count > 120 {
                                 self.highResFrameCaptures.removeFirst()
                             }
                             
@@ -499,7 +499,7 @@ public class LiveStageViewer {
 class RateLimiter {
     
     private let syncQueue = DispatchQueue(label: "com.samsoffes.ratelimit", attributes: [])
-    public let limit: TimeInterval = 0.1
+    public let limit: TimeInterval = 0.5
     public private(set) var lastExecutedAt: Date?
     
     func feed(block: ()->Void) {
